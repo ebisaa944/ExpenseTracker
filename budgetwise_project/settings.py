e34dab0148c42f765b336203689eea301eb0ba44
ebisaa944@ <ebisaa944@gmail.com>
2025-10-27 17:01:54 +0300

    Second commit

--- budgetwise_project/settings.py
+++ budgetwise_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -113,7 +114,10 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Define where Django should look for static files inside your apps
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
